b62d806b3b1dd13806d560f90e4b01d79dc5a68a
kavyasri1230 <69074566+kavyasri1230@users.noreply.github.com>
2021-06-12 12:21:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var p,t,r,si;
 p = document.getElementById ("first").value;
 r = document.getElementById ("second").value;
 t = document.getElementById ("third").value;
+if(p<=0){
+alert("Enter a positive number");
+}
 si = parseInt((p*t*r)/100 );
 document.getElementById ('num').innerHTML ="Simple interest : "+si;
 }
